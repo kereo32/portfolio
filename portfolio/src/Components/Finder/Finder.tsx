1da0926c667edb27cbe358716ccfc8cf2f9eeeee
Kerem Hacisabanoglu <kerem.hacisabanoglu@adcolony.com>
2024-01-14 23:01:56 +0300

    Fix Finder component layout and add iPhone preview to ProjectPreview

--- portfolio/src/Components/Finder/Finder.tsx
+++ portfolio/src/Components/Finder/Finder.tsx
@@ -9,10 +9,10 @@ type FinderProps = {
   changeTitle: (title: string) => void;
 };
 
-const Finder = ({ title, children, isOpen,changeTitle }: FinderProps) => {
+const Finder = ({ title, children, isOpen, changeTitle }: FinderProps) => {
   return (
-    <div className={`transition-transform transform ${isOpen ? 'scale-y-100 scale-x-100' : 'scale-y-0 scale-x-0'} flex flex-row w-[70%] h-[80%]`}>
-      <FinderSideBar changeTitle={changeTitle} title={title}/>
+    <div className={`transition-transform transform ${isOpen ? 'scale-y-100 scale-x-100' : 'scale-y-0 scale-x-0'} flex flex-row min-w-[70%] h-[80%]`}>
+      <FinderSideBar changeTitle={changeTitle} title={title} />
       <FinderContentArea title={title} children={children} />
     </div>
   );
--- portfolio/src/Hooks/useProjectPreviewPopUp.tsx
+++ portfolio/src/Hooks/useProjectPreviewPopUp.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import close from '/images/close.png';
+import phone from '/images/phone.png';
 
 interface ProjectPreviewHook {
   title: string;
@@ -7,7 +8,7 @@ interface ProjectPreviewHook {
   url: string;
 }
 
-const useProjectPreviewPopUp = ({ title, description,url }: ProjectPreviewHook) => {
+const useProjectPreviewPopUp = ({ title, description, url }: ProjectPreviewHook) => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   const openPopup = () => {
@@ -19,17 +20,32 @@ const useProjectPreviewPopUp = ({ title, description,url }: ProjectPreviewHook) 
   };
 
   const Popup = () => (
-    <div className={`absolute w-[80%] h-[80%] z-60 bg-black bg-opacity-75 rounded-md`}>
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[100%] h-full bg-black bg-opacity-75 rounded-md z-60">
       <div className="flex flex-col w-full h-full justify-center items-center text-center text-white">
         <div className='flex flex-row w-full h-[10%] justify-end mr-4'>
-        <button onClick={closePopup}>
-          <img src={close} alt="close" className="w-4 invert m-4" />
-        </button>
+          <button onClick={closePopup}>
+            <img src={close} alt="close" className="w-4 invert m-4" />
+          </button>
         </div>
-        <div className='flex flex-col w-full h-[80%] justify-center items-center'>
-        <h2 className=' font-bold'>{title}</h2>
-        <p className='m-4' dangerouslySetInnerHTML={{ __html: description.replace(/\n/g, '<br />') }} />
-        <iframe src={encodeURI(url)} className='w-full h-full' />
+        <div className='flex flex-col w-full h-[80%] justify-start items-center overflow-y-auto'>
+          <div className='flex flex-col w-full h-[20%] justify-center items-start space-y-4 ml-6'>
+            <h1 className='font-bold'>{title}</h1>
+            <h2 className='font-mono' dangerouslySetInnerHTML={{ __html: description.replace(/\n/g, '<br />') }} />
+          </div>
+          <div className="flex flex-col w-[75%] h-[70%] justify-start items-center relative">
+            <img
+              src={phone}
+              alt="iPhone Background"
+              className="w-[95%] h-[95%] absolute inset-0"
+            />
+            <div className="relative w-[78%] h-[71%] mt-4">
+              <iframe
+                title="iPhone Screen"
+                src={encodeURI(url)}
+                className="w-full h-full border-none rounded-3xl"
+              ></iframe>
+            </div>
+          </div>
         </div>
       </div>
     </div>
